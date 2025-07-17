c339d000a464a1c1dbf14b4a684eba61b8ae643a
DanSheshtanov <33222142+DanSheshtanov@users.noreply.github.com>
2025-07-17 19:39:35 +0100

    Added log function that prints directly without line and file

--- src/Debug.h
+++ src/Debug.h
@@ -7,6 +7,11 @@
 #define LOG(msg) \
 	OutputDebugStringA((std::string(__FILE__) + "(" + std::to_string(__LINE__) \
 		+ "): " + std::string(msg) + "\n").c_str())
+
+#define LOGDIRECT(msg) \
+	OutputDebugStringA((std::string(msg) + "\n").c_str());
+
 #else
-#define LOG(s)
+#define LOG(msg)
+#define LOGDIRECT(msg)
 #endif // _DEBUG
