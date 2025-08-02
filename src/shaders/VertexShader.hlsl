97f4cc0647fcf471622188c261f366de1aa2f5a9
DanSheshtanov <33222142+DanSheshtanov@users.noreply.github.com>
2025-08-02 18:31:12 +0100

    Removed commented out lines from VertexShader.hlsl

--- src/shaders/VertexShader.hlsl
+++ src/shaders/VertexShader.hlsl
@@ -8,19 +8,9 @@ struct VOut
     float2 uv : TEXCOORD;
 };
 
-//cbuffer CBuffer0 : register(b0)
-//{
-//    float time;    // 12 bytes
-//    float3 packing;  // 4 bytes (no overlap on 16 byte boundary)
-//    //float4 col;     // 16 bytes
-//};
-
-
-
 VOut main(VIn input)
 {
     VOut output;
-    //output.position = mul(WVP, float4(input.position, 1));
     float scale = (sin(input.position.x * 10 + time) / 2.0) + 1;
     output.position = mul(WVP, float4(input.position, 1));
     output.position.y += scale;
