aec4d0089eb40349a2e01f1ee215758a6c74b19f
DanSheshtanov <33222142+DanSheshtanov@users.noreply.github.com>
2025-08-02 19:39:11 +0100

    Vertex shader waving removed and reintroduced into dedicated shader

--- src/shaders/VShaderWaves.hlsl
+++ src/shaders/VShaderWaves.hlsl
@@ -8,10 +8,18 @@ struct VOut
     float2 uv : TEXCOORD;
 };
 
+cbuffer CBuffer
+{
+    float waveSpeed;
+    float3 padding;
+};
+
 VOut main(VIn input)
 {
     VOut output;
+    float scale = (sin(input.position.x * 10 + time * waveSpeed) / 2.0) + 1;
     output.position = mul(WVP, float4(input.position, 1));
+    output.position.y += scale;
     output.colour = float4(1, 1, 1, 1);
     output.uv = input.uv;
     return output;
